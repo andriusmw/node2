cc6fd038a1a755a7a3842941161393e384977ff5
andriusmw <andriusmw@yahoo.es>
2022-05-27 15:49:38 +0200

    modificaciones de lo mismo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 //para ver los comandos que le introducimos al llamar el fichero index.js ->process.js
 //en el video.
 
+const chalk = require("chalk");
 const minimist = require("minimist");
 //requiere el modulo minimist y este chekea los argumentos con los que se llama al programa
 
@@ -12,7 +13,9 @@ console.log(args);
 //Muestra lo mismo que la linea 3 pero mas bonito, lo mete en un objeto.
 
 if (!args.inputDir || !args.outputDir) {
-  console.error("los argumentos --inputDir y --outputDir son obligatorios");
+  console.error(
+    chalk.red("los argumentos --inputDir y --outputDir son obligatorios")
+  );
   process.exit(1);
   //chekea cuando se ejecuta si en los argumentos existe inputdir y outpurdir.
   //sin no existen muestra el error y manda el codigo 1 a la salida del proceso
@@ -20,7 +23,29 @@ if (!args.inputDir || !args.outputDir) {
 }
 
 if (!args.watermark && !args.resize) {
-  console.error("Es necesario alguno de los argumentos --watermark o --resize");
+  console.error(
+    chalk.red("Es necesario alguno de los argumentos --watermark o --resize")
+  );
 }
 
-console.log("empezamos a procesar las imágenes");
+console.log(chalk.green("empezamos a procesar las imágenes"));
+//chalk.green() le aplica un filtro al texto para que se muestre de un color u otro
+
+const { watermark, resize, inputDir, outputDir } = args;
+//destructuring del objeto args para autocomplete y más comodidad al rescribir
+
+/************************************************************** */
+
+async function processImages({ inputDir, outputDir, watermark, resize }) {
+  console.log(inputDir, outputDir, watermark, resize);
+  //comprobar que existe inputDir
+  //Crear si no existe outputDIr
+  //Comprobar si existe watermark
+  //Leer contenidos de inputDir
+  //Quedarme solo con los archivos que sean imagenes
+  //si existe resize redimensionar
+  //si existe watermak poner marca de agua
+  //guardar la imagen resultante en outputdir
+}
+
+processImages({ inputDir, outputDir, watermark, resize });
