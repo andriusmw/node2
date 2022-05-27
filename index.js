19b263d5319ec010477351d49f28c611702bd575
andriusmw <andriusmw@yahoo.es>
2022-05-27 15:15:47 +0200

    monstrando argumentos con minimist

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log(process.argv);
+/****************** preparativos ************************/
+
+//console.log(process.argv);
 //para ver los comandos que le introducimos al llamar el fichero index.js ->process.js
 //en el video.
+
+const minimist = require("minimist");
+//requiere el modulo minimist
+
+const args = minimist(process.argv);
+console.log(args);
+//Muestra lo mismo que la linea 3 pero mas bonito, lo mete en un objeto.
