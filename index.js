e60e67faeede75888eb6f83d174c606dc55675a9
andriusmw <andriusmw@yahoo.es>
2022-05-27 15:24:22 +0200

    requisitos obligatorios argumentos + chaclk 4.1.2

--- index.js
+++ index.js
@@ -5,8 +5,22 @@
 //en el video.
 
 const minimist = require("minimist");
-//requiere el modulo minimist
+//requiere el modulo minimist y este chekea los argumentos con los que se llama al programa
 
 const args = minimist(process.argv);
 console.log(args);
 //Muestra lo mismo que la linea 3 pero mas bonito, lo mete en un objeto.
+
+if (!args.inputDir || !args.outputDir) {
+  console.error("los argumentos --inputDir y --outputDir son obligatorios");
+  process.exit(1);
+  //chekea cuando se ejecuta si en los argumentos existe inputdir y outpurdir.
+  //sin no existen muestra el error y manda el codigo 1 a la salida del proceso
+  //(eso se usa en el futuro con otras apps que recogen ese código)
+}
+
+if (!args.watermark && !args.resize) {
+  console.error("Es necesario alguno de los argumentos --watermark o --resize");
+}
+
+console.log("empezamos a procesar las imágenes");
